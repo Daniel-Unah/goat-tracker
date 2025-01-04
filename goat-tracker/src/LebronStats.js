352fc15c1b117edc43a807483996289ac71acb93
Daniel Unah <dunah09@gmail.com>
2025-01-04 13:37:36 -0500

    pushing requirements.txt

---
+++ goat-tracker/src/LebronStats.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const LebronStats = () => {
+  const PATHWAY = 'https://goat-tracker.onrender.com/'
+  const [stats, setStats] = useState([]);
+}
+
+export default LeBronStats;
