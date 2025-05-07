2a1b18d4765392f7cbd88bad97ca2b94cb0a9095
Daniel <106980147+Daniel-Unah@users.noreply.github.com>
2025-05-07 12:22:49 -0400

    updated random game pathway to loop through every season instead of just the most recent season, added playoff and regular season toggle

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from nba_api.stats.endpoints import playercareerstats, playergamelog
 from nba_api.stats.static import players
+import pandas as pd
 
 
 
@@ -23,18 +24,25 @@ def get_lebron_career_stats():
 
     return jsonify(stats_df.to_dict(orient='records'))
 # route to lebron's stats his most recent game
-@app.route('/api/lebron/recent-game', methods=['GET'])
+@app.route('/api/lebron/recent-game?season_type=Playoffs', methods=['GET'])
 def get_lebron_game_stats():
-    game_stats = playergamelog.PlayerGameLog(player_id=player_id, season='2024-25')
+    season_type = request.args.get('season_type', default='Regular Season')
+    game_stats = playergamelog.PlayerGameLog(player_id=player_id, season='2024-25', season_type_all_star=season_type)
     stats_df = game_stats.get_data_frames()[0]
 
     return jsonify(stats_df.to_dict(orient='records')[0])
 # route to lebron's stats from a random notable game
-@app.route('/api/lebron/random-game', methods=['GET'])
+@app.route('/api/lebron/random-gameseason_type=Playoffs', methods=['GET'])
 def get_lebron_random_game_stats():
-    career_game_stats = playergamelog.PlayerGameLog(player_id=player_id)
-    stats_df = career_game_stats.get_data_frames()[0]
-    notable_games = stats_df[stats_df['PTS'] + stats_df['REB'] + stats_df['AST'] >= 50]
+    season_type = request.args.get('season_type', default='Regular Season')
+    # Notable games are defined as games where the sum of points, rebounds, and assists is greater than or equal to 50
+    all_stats_df = pd.DataFrame()
+    for year in range(2003, 2024):
+        season = f"{year}-{str(year+1)[-2:]}"
+        season_stats = playergamelog.PlayerGameLog(player_id=player_id, season=season, season_type_all_star=season_type)
+        stats_df = season_stats.get_data_frames()[0]
+        all_stats_df = pd.concat([all_stats_df, stats_df], ignore_index=True)
+    notable_games = all_stats_df[all_stats_df['PTS'] + all_stats_df['REB'] + all_stats_df['AST'] >= 50]
     random_game = notable_games.sample(n=1).iloc[0]
     return jsonify(random_game.to_dict())
 
--- goat-tracker/src/RecentGameStats.js
+++ goat-tracker/src/RecentGameStats.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import "./RecentGameStats.css";
 
 function RecentGameStats() {
-    const PATHWAY = 'http://127.0.0.1:5000/api/lebron/recent-game';
     const [recentGameStats, setRecentGameStats] = useState([]);
+    const [seasonType, setSeasonType] = useState("Regular Season");
 
     useEffect(() => {
+        const PATHWAY = `http://127.0.0.1:5000/api/lebron/recent-game?season_type=${seasonType}`;
         const fetchData = async () => {
             try {
                 const response = await fetch(PATHWAY);
@@ -17,13 +18,20 @@ function RecentGameStats() {
             }
         };
         fetchData();
-    }, []);
+    }, [seasonType]); // this makes the data refresh whenever seasonType changes
 
     return (
-        <div className="RecentGameStats">
-            <p>In Lebron James' most recent game, he scored {recentGameStats.PTS} points, grabbed {recentGameStats.REB} boards, and assisted {recentGameStats.AST} times in {recentGameStats.MIN} minutes in a {recentGameStats.WL}.
-            </p>
-        </div>
+        <>
+            <select value={seasonType} onChange={(e) => setSeasonType(e.target.value)}>
+                <option value="Regular Season">Regular Season</option>
+                <option value="Playoffs">Playoffs</option>
+            </select>
+
+            <div className="RecentGameStats">
+                <p>In Lebron James' most recent game, he scored {recentGameStats.PTS} points, grabbed {recentGameStats.REB} boards, and assisted {recentGameStats.AST} times in {recentGameStats.MIN} minutes in a {recentGameStats.WL}.
+                </p>
+            </div>
+        </>
     )
 }
 
